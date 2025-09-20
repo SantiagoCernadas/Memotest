4a29484c406682db6950e269f1b8648e8e14f7d6
santiagoCernadas <santiagocernadas29@gmail.com>
2025-09-20 11:13:40 -0300

    se enlaza script y se empieza a modificar DOM

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let cartas = [
+document.getElementById('carta_1'),
+document.getElementById('carta_2'),
+document.getElementById('carta_3'),
+document.getElementById('carta_4'),
+document.getElementById('carta_5'),
+document.getElementById('carta_6'),
+document.getElementById('carta_7'),
+document.getElementById('carta_8'),
+document.getElementById('carta_9'),
+document.getElementById('carta_10'),
+document.getElementById('carta_11'),
+document.getElementById('carta_12'),
+document.getElementById('carta_13'),
+document.getElementById('carta_14'),
+document.getElementById('carta_15'),
+document.getElementById('carta_16'),
+];
